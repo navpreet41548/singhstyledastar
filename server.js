5f28f10702c5c4bc939758c54abedfaf6c3f6229
Navpreet singh <navpreetsingh41548@gmail.com>
2021-08-05 09:30:56 -0700

    third commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://singhstyleksndfk3409sdfnsdfk23.herokuapp.com",
     credentials: true,
 }))
 
